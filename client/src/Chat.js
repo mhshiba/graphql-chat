5e79006a4a51a3991ab81e9b6ed34035521eebdd
Marcelo Shiba <marcelo.shiba@pipefy.com>
2021-11-15 08:52:35 -0300

    :bulb: Add some comments

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -9,9 +9,13 @@ const Chat = ({user}) => {
   // Poderia/deveria usar o loading e error para tratamento diferenciados
   // enquanto os dados ainda não estão prontos
   const { data } = useQuery(messagesQuery);
+  console.log('data', data);
   const messages = data ? data.messages: [];
   useSubscription(messageAddedSubscription, {
     onSubscriptionData: ({client, subscriptionData}) => {
+      // Atualiza o cache, e então o componente é reenderizado
+      // E com o cache atualizado, o `useQuery(messagesQuery)` vai
+      // usar esses dados recém atualizados
       client.writeData({data: {
         messages: messages.concat(subscriptionData.data.messageAdded)
       }})
